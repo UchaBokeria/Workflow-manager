527e8b5b42d6ebb8d78cd2deb2ef0893381928c3
FeRla17X <ucha2bokeria@mail.com>
2021-01-10 17:25:39 -0800

    Ich mag sass zu sehr das ist fantastisch..
    P.S API link:  API LINK: http://tasksapi.docelove.cf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WorkflowManager';
+  
+  // API LINK: http://tasksapi.docelove.cf 
+  
+  // DABLA DAWERILI KODI UNDA GADAVIDES SHESABAMIS CHILD COMPONENTSHI
+  
+  sorting = false;
+  sortingDrop = "";
+  editMode = false;
+  checkAll = false;
+  
+  // DABLA MOCEMULI TASK -> ARRAY UNDA CHAANACVLO API -TI MOTANILI DATA-TI
+  
+  Tasks = [
+  {title:"Workout",description:" Lorem tu ragac",done:true,starts:"02:35",ends:"03:35"},
+  {title:"Workout",description:" Lorem tu ragac",done:false,starts:"02:35",ends:"03:35"},
+  {title:"Workout",description:" Lorem tu ragac",done:false,starts:"02:35",ends:"03:35"},
+  {title:"Workout",description:" Lorem tu ragac",done:false,starts:"02:35",ends:"03:35"}
+  ];
+  
+  CheckAll(){
+    for (let i = 0; i < this.Tasks.length; i++)
+      this.Tasks[i].done = false;
+      
+    for (let i = 0; i < this.Tasks.length; i++) 
+      this.Tasks[i].done = this.checkAll;
+    
+    this.checkAll = !this.checkAll;
+  }
+  
+  Done(){}
+  Undone(){}
+  Clear(){}
+  sort(){}
+  search(){}
 }
